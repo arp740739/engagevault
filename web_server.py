be94cdc6abec0e89fb24d27dc1b74f04c4d559d2
EngageVault <arp740739@gmail.com>
2024-12-31 23:01:47 +0100

    Simplify code for deployment debugging

--- web_server.py
+++ web_server.py
@@ -1,20 +1,8 @@
-from flask import Flask, render_template, jsonify, session, redirect, url_for
-from authlib.integrations.flask_client import OAuth
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'dev-key-123')
-
-oauth = OAuth(app)
-oauth.register(
-    name='twitter',
-    client_id=os.environ.get('TWITTER_CLIENT_ID'),
-    client_secret=os.environ.get('TWITTER_CLIENT_SECRET'),
-    api_base_url='https://api.twitter.com/2/',
-    access_token_url='https://api.twitter.com/2/oauth2/token',
-    authorize_url='https://twitter.com/i/oauth2/authorize',
-    client_kwargs={'scope': 'tweet.read users.read follows.read'}
-)
 
 @app.route('/')
 def index():
