3982ea6f45f304693747df3c41ded677c5e68c84
EngageVault <arp740739@gmail.com>
2024-12-31 23:17:46 +0100

    Add basic template with styles

--- web_server.py
+++ web_server.py
@@ -1,11 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello from EngageVault!"
-
-# Ne pas inclure cette partie pour le déploiement
-# if __name__ == '__main__':
-#     app.run()
+    return render_template('tasks.html')
